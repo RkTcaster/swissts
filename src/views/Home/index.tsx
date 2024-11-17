c278d49551f8c870e834663c5533a5a253d6acd8
Lautaro M <malautaro@gmail.com>
2024-11-17 13:45:17 -0300

    wip addPlayer and input

--- src/views/Home/index.tsx
+++ src/views/Home/index.tsx
@@ -1,11 +1,41 @@
+import { Player } from '@/classes/Player'
 import Card from '@/components/Card'
+import { useState } from 'react'
 
 const Home = () => {
+  const [players, setPlayers] = useState<string[]>(['Player 1'])
+  const handlePlayerNameChange = ({ name, index }: { name: string; index: number }) => {
+    setPlayers((prevPlayers) => {
+      return prevPlayers.map((player, playerIndex) => {
+        if (playerIndex === index) {
+          return name
+        }
+        return player
+      })
+    })
+  }
+  const handleAddPlayer = () => {
+    setPlayers((prevPlayers) => {
+      return [...prevPlayers, '']
+    })
+  }
+  console.log(players)
 
   return (
     <section className='flex items-center justify-center mt-6 container text-[14px]'>
       <Card>
-        <div className='flex flex-col gap-4'>Hello World</div>
+        {players.map((player, i) => {
+          return (
+            <input
+              type='text'
+              value={player}
+              onChange={(e) => {
+                handlePlayerNameChange({ name: e.target.value, index: i })
+              }}
+            />
+          )
+        })}
+        <button onClick={handleAddPlayer}>Add Player</button>
       </Card>
     </section>
   )
